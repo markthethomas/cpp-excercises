69a23a679eccab608f3751ce9d35b503c72cc749
Mark Thomas <markthethomas@gmail.com>
2015-02-03 19:32:50 -0800

    Add first homework set

---
+++ homework/homework1.cpp
@@ -0,0 +1,16 @@
+// HW
+// In ancient India, a simple algorithm was used to compute the length of the hypotenuse of a right triangle. Let  h be the hypotenuse and let a and b be the two other sides of the triangle. Select the shorter side to be a, and let the longer side be b. Then the length of the hypotenuse is calculated as h = a + b/2.  For example, let the two sides of a right triangle be 3 and 4. Then a =3, and b = 4.  And h = a + b/2 = 3 + 4/2 = 3 + 2 = 5.
+// Following all guidelines discussed in class, write a program to calculate the hypotenuse by using the above approximation method. Test your program for 5 different right triangles. For each triangle, also calculate the hypotenuse using the Pythagoras formula and compute the difference between the two results for each triangle.  Note that the user can enter the sides in any order. Your program must decide which is the longer side.
+// Sample message and user input:
+// Enter the two sides of a right triangle separated by a space:  3 4
+// Sample output:
+// Shorter side	Longer side 	Hypotenuse	Pythagoras method	Difference
+// 3		4		5		5			0
+// 1		1		1.500		1.414			0.086
+//
+// Submissions:
+// Your source (.cpp) file
+// Word File containing a screen shot of your output. To get a screen capture, use Alt+Prt Screen. Paste inside the Word file.
+// DUE:  2/17/2015. NO LATER THAN 5.00 pm
+//
+//
