ca6a246b4e3e966fdbe5b4736447d4a83d4d1ce6
Mark Thomas <markthethomas@gmail.com>
2015-02-03 18:33:39 -0800

    adding everything in

--- chapter_3/program.cpp
+++ chapter_3/program.cpp
@@ -2,6 +2,8 @@
 
 #include <iostream>
 #include <iomanip>
+#include <typeinfo>
+
 using namespace std;
 
 int main() {
@@ -9,14 +11,16 @@ int main() {
     double readRate;
     int sum = 5;
 
-    cout << "How many books do you plan to read? ";
-    cin >> books;
+    // cout << "How many books do you plan to read? ";
+    // cin >> books;
+    //
+    // cout << "how many months will it take you to read? ";
+    // cin >> months;
+    //
+    // readRate = static_cast<double>(books) / months;
+    // cout << "That is" << readRate << " books per month.\n";
 
-    cout << "how many months will it take you to read? ";
-    cin >> months;
-
-    readRate = static_cast<double>(books) / months;
-    cout << "That is" << readRate << " books per month.\n";
-    cout << (typeid(sum));
+    cout << typeid(sum).name();
 
 }
+  
